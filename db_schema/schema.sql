3ad9b120a02d1de33e0fbf5ce6278408bc48cd1e
DanielZerihunGeda <lenjisa48@gmail.com>
2024-01-04 10:07:07 +0300

    loader

--- db_schema/schema.sql
+++ db_schema/schema.sql
@@ -1,4 +1,3 @@
-CREATE TABLE VectorDatabaseTable1 (
-    "City Name" VARCHAR(255) PRIMARY KEY,
-    VectorizedFeatures FLOAT[],   -- Combined vector representation for all features (Date, Cities, Views)
-);
+
+
+
